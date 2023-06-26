0dca823142ae5bd59583d18d5dca651a5c1451d6
TocoToucan <locomopp@gmail.com>
2023-06-26 17:30:16 +0900

    Initial Commit

---
+++ MoonshotDemo/MoonshotDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotDemoApp.swift
+//  MoonshotDemo
+//
+//  Created by Minseok Choi on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
